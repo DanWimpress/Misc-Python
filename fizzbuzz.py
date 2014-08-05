fc9d9084f51bea061cbde98f41d13ada221c9e51
DanWimpress <daniel.wimpress@gmail.com>
2014-08-05 17:04:28 +0100

    Refactor
    removed checks that were not needed

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -5,9 +5,9 @@ def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
             print (str(i) + " fizzbuzz")
-        elif i % 3 == 0 and i % 5 != 0:
+        elif i % 3 == 0:
             print (str(i) + " fizz")
-        elif i % 3 != 0 and i % 5 == 0:
+        elif i % 5 == 0:
             print (str(i) + " buzz")
         else:
             print (str(i))
